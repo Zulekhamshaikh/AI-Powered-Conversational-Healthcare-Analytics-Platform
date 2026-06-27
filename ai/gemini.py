5e1e69cc1bcdee8142f41c12aa8703717f7600d1
zulekha <zulekhashaikh016@gmail.com>
2026-06-27 12:43:49 +0530

    Support Streamlit Secrets

--- ai/gemini.py
+++ ai/gemini.py
@@ -1,13 +1,14 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 from ai.prompt import SYSTEM_PROMPT
 
-# Load .env file
+# Load .env (works locally)
 load_dotenv()
 
-# Read API key
-API_KEY = os.getenv("GEMINI_API_KEY")
+# Use Streamlit Secrets if deployed, otherwise .env
+API_KEY = st.secrets.get("GEMINI_API_KEY", os.getenv("GEMINI_API_KEY"))
 
 # Create Gemini client
 client = genai.Client(api_key=API_KEY)
@@ -28,7 +29,6 @@ User Question
 """
 
     try:
-
         response = client.models.generate_content(
             model="gemini-2.5-flash",
             contents=prompt
@@ -37,5 +37,4 @@ User Question
         return response.text
 
     except Exception as e:
-
         return f"AI service is temporarily unavailable.\n\nDetails:\n{e}"
